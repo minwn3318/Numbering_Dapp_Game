bca9d258e90992e249808a9a924ad9dbd9950fde
minwn3318 <minwn3318@naver.com>
2024-07-11 21:51:29 +0900

    pygame logic modify - ranking logic

--- RankingGame_SmartContract.py
+++ RankingGame_SmartContract.py
@@ -1,77 +1,113 @@
 import random
+import pygame
+import sys
+import requests
+import json
+import pandas as pd
+import sqlite3
 
-def Rankig_Game():
-    playGame = True
-    print("+----------------+")
-    print("+ Numbering Game +")
-    print("+----------------+ \n")
-    while(playGame) :
-        print("(1)input 'S' to start Game\n(2) input 'M', show manual\n(3)input 'R', show Ranking\n(4) input 'Q', exit game\n")
-        input_key = input(": ")
-        if input_key == "S" :
-            difficult_loop = True
-            ranking_data = {
-                    "Difficalty" : None,
-                    "Name" : None,
-                    "Number" : None,
-                    "Try" : None,
-                }
-            number = None
+pygame.init()
 
-            while(difficult_loop) :
-                print("select difficalt name\n(1) easy - (0 ~ 100), (2) nomal - (0 ~ 500), (3) hard - (0~1000)")
-                difficult = input(": ")
-                ranking_data["Difficalty"] = difficult
-                if difficult != "easy" and difficult != "nomal" and difficult != "hard" :
-                    print("please input again")
-                else :
-                    max = None
-                    if difficult == "easy" :
-                        max = 100
-                    elif difficult == "nomal" :
-                        max = 500
-                    else :
-                        max = 1000
-                    number = random.randint(0,max)
-                    print(number)
-                    ranking_data["Number"] = number
-                    difficult_loop = False
+def return_key():
+    for event in pygame.event.get() :
+        return event
 
-            print("put your name")
-            user_name = input(": ")
-            ranking_data["Name"] = user_name
-            print(ranking_data)
-            difficult_loop = True
+def making_RankingData():
+    ranking_data = {
+            "Difficalty" : None,
+            "Name" : None,
+            "Number" : None,
+            "Try" : None,
+        }
+    return ranking_data
 
-            count = 1
-            while(True):
-                print("Guess Numger! :")
-                guess = int(input())
-                if guess == ranking_data["Number"] :
-                    print("correct!!")
-                    print(count)
-                    ranking_data["Try"] = count
-                    count = 0
-                    break
-                elif guess < ranking_data["Number"] :
-                    print("UP!!")
-                else  :
-                    print("Down!!")
-                count = count + 1
-            print(ranking_data)
+def select_Difficulty(ranking_data, event) :
+    max = None
+    difficult = None
 
-        elif input_key == "M" :
-            print("+----------------+")
-            print("+     manual     +")
-            print("+----------------+ \n")
-        elif input_key == "R" :
-            print("+----------------+")
-            print("+     ranking    +")
-            print("+----------------+ \n")
-        elif input_key == "Q" :
-            print("+----------------+")
-            print("+    exit Game   +")
-            print("+----------------+ \n")
-            playGame = False
+    if event.key == pygame.K_1 :
+        difficult == "easy"
+        max = 100
+    elif event.key == pygame.K_2:
+        difficult == "nomal" 
+        max = 500
+    elif event.key == pygame.K_3 :
+        difficult == "nomal" 
+        max = 1000
 
-Rankig_Game()
+    if difficult != None and max != None :
+        number = random.randint(0,max)
+        ranking_data["Number"] = number
+        return ranking_data
+
+def input_UserName(ranking_data, event, str_name) :
+    if event.unicode.isalpha():
+        str_name = str_name + event.unicode
+    elif event.key == pygame.K_KP_ENTER or event == pygame.KSCAN_KP_ENTER :
+        ranking_data["Name"] = str_name
+        return ranking_data
+
+def playing_Game(ranking_data, event, number, count) :
+    if event.unicode.isdigit():
+        number = number + event.unicode
+
+    if event.key == pygame.K_KP_ENTER or event == pygame.KSCAN_KP_ENTER :
+        if number == ranking_data["Number"] :
+            print("correct!!")
+            print(count)
+            ranking_data["Try"] = count
+            count = 0
+            return ranking_data
+
+        else :
+            if number < ranking_data["Number"] :
+                print("UP!!")
+            else  :
+                print("Down!!")
+
+            number = ""
+            count = count + 1
+
+def transmit_rankingTransaction(ranking_data) :
+    data = {
+            "sender": "user",
+            "recipient": "user",
+            "amount": 0,
+            "smart_contract" : {
+                "ranking" : ranking_data
+            }   
+        }
+    headers = {'Content-Type' : 'application/json; charset=utf-8'}
+    requests.post("http://127.0.0.1:5000/transactions/new", headers=headers, data=json.dumps(data))
+
+type_ranking = []
+easy_ranking = None
+nomal_ranking = None
+hard_ranking = None
+
+def show_ranking() :
+    headers = {'Content-Type' : 'application/json; charset=utf-8'}
+    value = requests.get("http://127.0.0.1:5000/chain", headers=headers)
+    for transaction in value :
+        if "ranking" in transaction :
+            type_ranking.append(transaction["smart_contract"]["ranking"]) 
+    
+    conn = sqlite3.connect('sample.db')
+    ranking_Frame = pd.DataFrame(type_ranking)
+
+    ranking_Frame.to_sql('ranking_list',conn)
+
+    easy_ranking = pd.read_sql_query("SELECT Difficalty, Name, Number, Try FROM ranking_list Where Difficalty = easy ORDER BY Try DESC LIMIT 10", conn)
+    nomal_ranking = pd.read_sql_query("SELECT Difficalty, Name, Number, Try FROM ranking_list Where Difficalty = easy ORDER BY Try DESC LIMIT 10", conn)
+    hard_ranking = pd.read_sql_query("SELECT Difficalty, Name, Number, Try FROM ranking_list Where Difficalty = easy ORDER BY Try DESC LIMIT 10", conn)
+    
+    print(easy_ranking)
+    print(nomal_ranking)
+    print(hard_ranking)
+
+
+def show_Manual():
+    print("\n게임설명 :\n숫자 범위에서 무작위로 선정된 숫자를 맞추면 이기는 게임입니다\n\n게임플레이방법 : \n(1) 먼저 난이도를 선택합니다\n(2) 선택한 난이도에 맞는 숫자 범위에서 무작위 숫자가 선택됩니다\n(3) 화면의 빈칸에 숫자를 입력하면 되며, 시스템은 입력한 숫자가 실제 숫자보다 작다면 up, 실제 숫자보다 크다면 down을 외칩니다.\n(4) 플레이어는 그에 맞게 다시 숫자를 조정하여 입력하면 됩니다.\n(5) 게임이 끝나게 되면 난이도, 플레이어의 이름, 숫자, 시도횟수가 출력된 후 최종적인 랭킹에 입력되고 랭킹화면을 보여주며 게임을 종료합니다")
+
+drawing_state = ["ready", "manual", "level", "name", "play", "ranking"]
+state = None
